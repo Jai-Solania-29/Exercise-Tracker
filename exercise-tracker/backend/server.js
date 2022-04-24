f85dd589c4eef3157c94ecde4a0aa0109b25502d
Jaideep2912 <jaideep2912@gmail.com>
2022-04-24 22:54:49 +0530

    Route Added

--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -11,11 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established succesfully");
-})
+mongoose.connect(uri, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+ }, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
+ });
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//     console.log("MongoDB database connection established succesfully");
+// })
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
